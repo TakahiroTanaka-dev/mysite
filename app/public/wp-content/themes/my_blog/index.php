7c3d1b29797489f2a74c53ed76a014e4f9bf7870
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-11-21 00:49:22 +0900

    絶対パスを適用

--- app/public/wp-content/themes/my_blog/index.php
+++ app/public/wp-content/themes/my_blog/index.php
@@ -11,15 +11,15 @@
   <title>Clean Blog - Start Bootstrap Theme</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="http://mysite.local/wp-content/themes/my_blog/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="http://mysite.local/wp-content/themes/my_blog/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
   <!-- Custom styles for this template -->
-  <link href="css/clean-blog.min.css" rel="stylesheet">
+  <link href="http://mysite.local/wp-content/themes/my_blog/css/clean-blog.min.css" rel="stylesheet">
 
 </head>
 
@@ -59,7 +59,7 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-            <h1>Clean Blog</h1>
+            <h1>Clean Blog<?php echo date('Y'); ?></h1>
             <span class="subheading">A Blog Theme by Start Bootstrap</span>
           </div>
         </div>
