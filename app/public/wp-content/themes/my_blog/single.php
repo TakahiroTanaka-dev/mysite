037633d329e8544637f586bd65570344cfe1b095
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-11-23 15:42:43 +0900

    single.phpにアイキャッチ画像が設定されていなければ、デフォルトの画像を設定るすように変更

--- app/public/wp-content/themes/my_blog/single.php
+++ app/public/wp-content/themes/my_blog/single.php
@@ -14,10 +14,14 @@
   <?php if(have_posts()): ?>
   <?php while(have_posts()): the_post(); ?>
   <!-- Page Header -->
-  <?php $id = get_post_thumbnail_id(); ?>
-  <?php $img = wp_get_attachment_image_src($id, 'large'); 
+  <?php if(has_post_thumbnail()) : ?>
+    <?php $id = get_post_thumbnail_id(); ?>
+    <?php $img = wp_get_attachment_image_src($id, 'large'); 
     // var_dump($img)
-  ?>
+    ?>
+  <?php else: ?>
+      <?php $img = array(get_template_directory_uri().'/img/post-bg.jpg'); ?>
+  <?php endif; ?>
   <header class="masthead" style="background-image: url('<?php echo $img[0]; ?>')">
     <div class="overlay"></div>
     <div class="container">
