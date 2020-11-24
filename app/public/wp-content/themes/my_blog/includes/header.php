cf8f84ebea653b9739f0b1d9744f43b427f5a264
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-11-24 22:35:44 +0900

    foreachを使って$itemに入っているtitleやurlを出力

--- app/public/wp-content/themes/my_blog/includes/header.php
+++ app/public/wp-content/themes/my_blog/includes/header.php
@@ -3,7 +3,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
   <div class="container">
-    <a class="navbar-brand" href="/">TakahiroTnaka</a>
+    <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo($show = 'name'); ?></a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       Menu
       <i class="fas fa-bars"></i>
@@ -21,7 +21,7 @@
       <ul class="navbar-nav ml-auto">
         <?php foreach ($menu_items as $item) : ?>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a>
+            <a class="nav-link" href="<?php echo esc_attr($item->url); ?>"><?php echo esc_html ($item->title); ?></a>
           </li>
         <?php endforeach; ?>
       </ul>
